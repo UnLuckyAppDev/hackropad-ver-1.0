6758a024429a2eed1b92db145db5a1cc80b063c0
UnLuckyAppDev <mahithchikoti@gmail.com>
2026-07-22 12:04:00 -0400

    rules.mk

--- firmware/hackropad/rules.mk
+++ firmware/hackropad/rules.mk
@@ -1,6 +1,5 @@
-# Set the MCU and board for the Seeed Studio XIAO RP2040
+# Set the MCU and board settings for the Seeed Studio XIAO RP2040
 MCU = RP2040
-BOARD = prima
 BOOTLOADER = rp2040
 
 # Enable Hack Club OLED Display module
